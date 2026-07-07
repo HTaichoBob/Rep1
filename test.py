58fca4096892e474b7189dfc9d85057eb77534b3
HTaichoBob <bobhill8901@gmail.com>
2026-07-06 21:49:41 -0400

    Python basics and output part 1

---
+++ test.py
@@ -0,0 +1,9 @@
+print ("Hello, I need your I.D. How old are you?")
+age = input ()
+print ("You are " + age + " Years old.")
+if int(age) < 18: print ("Access Denied. Too young!")
+if int(age) == 18: print ("You can come in, but no drinking!")
+if int(age) == 19: print ("You can come in, but no drinking!")
+if int(age) == 20: print ("You can come in, but no drinking!")
+if int (age) >=21: print ("Welcome! Enjoy your night")
+
---
+++ types_and_var.py
@@ -0,0 +1,19 @@
+name = "Bobby"
+age = 21
+height = 6.1
+width = 5.5
+print (f"Hello, my name is {name}. I am {age} years old. I am  {height} feet tall.")
+
+name = str(input("Who are you? "))
+age = int(input("How old are you?"))
+height = float(input("How tall are you?"))
+print (f"Hello, my name is {name}. I am {age} years old. I am  {height} feet tall.")
+
+print (f"In 5 years, I will be {age + 5} years old.")
+
+print (f"The area of a {width} x {height} rectangle is {width * height} ." )
+
+       
+
+
+
