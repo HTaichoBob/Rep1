59e910c9322056dc464a725d1caca643e1c08952
HTaichoBob <bobhill8901@gmail.com>
2026-07-07 21:54:25 -0400

    Fun word

---
+++ string_fun.py
@@ -0,0 +1,6 @@
+funny_word = str(input("Enter a funny word: "))
+print(f"Your funny word is: {funny_word}")
+print(f"Your funny word's length is: {len(funny_word)}")
+print(f"Your funny word in uppercase is: {funny_word.upper()}")
+print(f"Your funny word repeated 3 times is: {funny_word} {funny_word} {funny_word}")
+
