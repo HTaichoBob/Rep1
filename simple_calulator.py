81c9222bde97d0b5c2ffcd327cdcf3736d74bba2
HTaichoBob <bobhill8901@gmail.com>
2026-07-07 21:43:56 -0400

    Simple Calculator

---
+++ simple_calulator.py
@@ -0,0 +1,17 @@
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
+
+if operator == "+":
+    print(first_number + second_number)
+elif operator == "-":
+    print(first_number - second_number)
+elif operator == "*":
+    print(first_number * second_number)
+elif operator == "/":
+    print(first_number / second_number)
+    
+
+
+
+
