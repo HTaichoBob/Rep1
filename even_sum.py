8e675c6584fa98e75398d3179cd126fa24908592
HTaichoBob <bobhill8901@gmail.com>
2026-07-12 22:17:08 -0400

    Even sum with for and while loops

--- even_sum.py
+++ even_sum.py
@@ -1,4 +1,28 @@
-my_list = list(range(1, 50))
-for number in my_list:
-    if number % 2 == 0:
-        print(number)
+# while loop
+total = 0
+index = 0
+
+my_list = list(range(1,51))
+
+while index<len(my_list):
+    if my_list[index] % 2 == 0:
+        total += my_list[index]
+    index += 1
+
+
+
+
+# for loop
+#my_list = list(range(1, 51))
+#for number in my_list:
+ #   if number % 2 == 0:
+  #      total += number
+        
+print(f"The sum of even numbers 1 to 50 is {total}")
+
+
+
+
+
+
+
