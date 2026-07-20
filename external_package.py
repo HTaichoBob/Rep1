9c39bc03b1d11b1eceaac6fa4fafbc778aaa7ab2
HTaichoBob <bobhill8901@gmail.com>
2026-07-20 19:54:56 -0400

    utilities

---
+++ external_package.py
@@ -0,0 +1,8 @@
+# requires colorama
+
+from colorama import Back, Cursor, Fore, Style
+
+print(Fore.GREEN + "This is the color commonly used for GO!")
+print(Fore.RED + "This is the color commonly used for STOP!")
+print(Fore.YELLOW + "This is the color commonly used for CAUTION")
+print(Style.RESET_ALL + "Colors have been reset")
---
+++ use_utilities.py
@@ -0,0 +1,5 @@
+from mypackage import utilities
+
+print(utilities.greet("Bobby"))
+print(utilities.factorial(5))
+
